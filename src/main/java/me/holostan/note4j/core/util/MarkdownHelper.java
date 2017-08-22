8f4c52fa49a87779df86203329febae3ae110473
EASAP\ghu <ghu@contractor.ea.com>
2017-08-22 16:46:40 +0800

    stash 8/22

--- src/main/java/me/holostan/note4j/core/util/MarkdownHelper.java
+++ src/main/java/me/holostan/note4j/core/util/MarkdownHelper.java
@@ -7,8 +7,35 @@ import com.github.rjeschke.txtmark.Processor;
  */
 public class MarkdownHelper {
 
+    /**
+     * convert markdown to html
+     * @param markdown
+     * @return
+     */
     public static String process(String markdown) {
         return Processor.process(markdown);
+    }
+
+    /*---
+    title: Hexo Markdown-it 简明语法手册
+    date: 2016-01-15 20:19:32
+    tags: [Markdown,LaTex,教程]
+    categories: 学习
+    toc: true
+    mathjax: true
+    ---*/
+    public static void generate(String title, String date, String[] tags, String[] categories, boolean toc, boolean mathjax, String mdbody) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("---\n")
+                .append("title: ").append(title).append("\n")
+                .append("date: ").append(date).append("\n")
+                .append("tags: ")
+                .append("categories: ")
+                .append("toc: ")
+                .append("mathjax: ")
+                .append("---\n")
+                .append("\n")
+                .append(mdbody);
     }
 
 }
